f577af0ad38b53af62a13ae0deb3f7c25283ac22
Game <67319010032@loeitech.ac.th>
2025-06-18 11:30:45 +0700

    first commit 888

--- backend/config.py
+++ backend/config.py
@@ -3,7 +3,7 @@ class Config:
     SECRET_KEY = 'your-secret-key'
     JWT_SECRET_KEY = 'your-jwt-secret'
     MONGODB_SETTINGS = {
-        'db': 'mario_notes',
+        'db': 'mario_notes888',
         'host': 'localhost',
         'port': 27017
     }
